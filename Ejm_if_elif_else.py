39edcbf01fd8d4c23b04a261ff6bd8e764b358dc
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 13:05:34 -0600

    Ejemplo 5. if,elif and else

---
+++ Ejm_if_elif_else.py
@@ -0,0 +1,20 @@
+
+# Estructura if...elif..else
+
+a = 5
+b = 7
+c = 9
+
+#Determinar cual número es el mayor.
+#Es la misma estructura "Si" que vimos en pSeint
+#if = si
+#else = sino
+
+# el operador '+' se utiliza para nir (concatenar) cadenas
+# cuado usamos ',' no se necesita una onversión explicita
+if a > b:
+    print("El mayor es 'a': ", a)
+elif b > c:
+    print("El mayor es 'b': ",b)
+else:
+    print("El mayor es 'c': ",c)
