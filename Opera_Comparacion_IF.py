66f2958ca0ff4c4bd8e2f74c19e3f330c9cfe87b
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 13:00:46 -0600

    Ejemplo 4. Operadores comparación/True,False

---
+++ Opera_Comparacion_IF.py
@@ -0,0 +1,31 @@
+#Hacer un ejercicio en Python que muestre 
+# la utilización práctica de cada uno 
+# de los operadores de comparación.
+# Operadores
+
+print("MENÚ")
+print("1- 10==10")
+print("2- 17!=20")
+print("3- 15>30")
+print("4- 15<20")
+print("5- 10>=20")
+print("6- 10<=20")
+print("-----------------------")
+op = input("Elija una opción: ")
+
+if op == "1":
+    print(10==10)
+elif op == "2":
+    print(17!=20)
+elif op == "3":
+    print(15>30)
+elif op == "4":
+    print(15<20)
+elif op == "5":
+    print(10>=20)
+elif op == "6":
+    print(10<=20)
+
+print("Fin del programa")
+
+
