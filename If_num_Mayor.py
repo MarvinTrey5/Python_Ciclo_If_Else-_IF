103489448293c922fd71b2e61b30fa1247470784
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 13:06:06 -0600

    Ejemplo 6. Num/Mayor

---
+++ If_num_Mayor.py
@@ -0,0 +1,12 @@
+"""
+   2 Variables de valores numéricos
+   verificar cuando es mayor o cuando es menor.
+"""
+a = 20
+b = 7
+
+if a < b:
+    print("Verdadero")
+else:
+    print("Falso")
+print("Fin del programa")
