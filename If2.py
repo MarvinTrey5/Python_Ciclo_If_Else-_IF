c54ffbae8d69022b3d16e85f93392902bfed1964
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 12:52:09 -0600

    Ejemplo 2.

---
+++ If2.py
@@ -0,0 +1,25 @@
+'''
+    Ejemplo 2 usaremos nada más if
+    En el cuál si es diferente o igual a 0.
+    entonces hará una división.
+    y creamos una variable el cual realizará 
+    un incremento.
+'''
+# Variable 1
+x = 50
+# Variable 2
+z = 10
+# Si x es diferente o igual a 0.
+if x !=0:
+    # la variable c definida almacenará la
+    # división.
+    c = (x/z)
+    # Mostramos cuánto vale c antes del
+    # incremento.
+    print(c)
+    # la variable b definida almacenará el
+    # incremento de + 1 a la variable c
+    b = c + 1
+    # Mostramos cuánto vale b despúes del
+    # incremento.
+    print(b)
