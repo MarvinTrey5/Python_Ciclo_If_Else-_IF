34bdb5cf409b5ca632e3999213f4902493b9b890
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 12:54:26 -0600

    Ejemplo 6. Ternario

---
+++ If6.py
@@ -0,0 +1,11 @@
+'''
+    Ejemplo 6 usaremos algo que se utilizaba en el lenguaje C.
+    El operador ternario se trata de que en una sola línea de código
+    van incluidas tanto el if y el else.
+    Pero en este caso lo utilizaremos en una salida print() todo irá
+    dentro del print().
+'''
+z = 2
+# La sintaxis es la siguiente:
+# "Mostra si cumple"  if condicion else " No se cumple"  
+print("Es Mayor" if z > 4 else "No lo es")
