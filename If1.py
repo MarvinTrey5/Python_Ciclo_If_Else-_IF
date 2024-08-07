0966183988ac429eb329a8793876584aba647198
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 12:51:52 -0600

    Ejemplo 1.

---
+++ If1.py
@@ -0,0 +1,20 @@
+'''
+    Ejemplo 1 usaremos nada más if
+    supongamos que tenemos 2 variable
+    y cada una almacena 1 valor númerico.
+    sabemos que un valor dividido entre 0 no es
+    lo correcto.
+    Entonces usaremos los condiciones como ejemplo
+    para realizar esa división.
+'''
+# variable 1
+x = 5
+#variable 2
+y = 7 
+# Si y que es 7 Ojo hay 2 condiciones
+# != Significa diferente o igual a 0
+# entonces si cumple
+if y != 0:
+    # Mostrará lo siguiente.
+    # Es decir hará una división.
+    print(x/y)
