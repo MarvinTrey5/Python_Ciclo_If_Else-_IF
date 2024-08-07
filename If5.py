d316867c6721b49fb42e1b362bcd5876de4df1df
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 12:53:58 -0600

    Ejemplo 5.

---
+++ If5.py
@@ -0,0 +1,13 @@
+'''
+    Ejemplo 5 usaremos nada más if solamente
+    usaremos un operador > pero no será práctico.
+    Simplemente es un error común de python si por el 
+    momento realizamos una línea de código y no queremos
+    codificar la parte if y nos aparecerá un error
+    para eliminarlo utilizamos la palabra clave pass.
+    Pass signfica le estamos diciendo a la línea de código
+    dejanos pasar a nuestra siguiente línea de código.
+'''
+x = 10
+if x > 2:
+    pass
