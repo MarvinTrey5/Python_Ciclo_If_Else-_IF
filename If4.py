791e0bc61cf56a2419db64c56594ab5bd4d576b0
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 12:53:44 -0600

    Ejemplo 4.

---
+++ If4.py
@@ -0,0 +1,14 @@
+'''
+    Ejemplo 4 usaremos nada más if con los operadores
+    lógicos > y el operador and.
+    Recordemos que para que el and ejecute las acciones
+    ambas condiciones deben ser verdaderas o True.
+'''
+# Vaiable 1.
+x = 8
+# La condición es la siguiente.
+# si la variable x que vale 8.
+# es mayor a 7 and x es menor a 10.
+if x > 7 and x < 10:
+    # Mostrará esté mensaje.
+    print("Es Mayor a 7 y menor a 10")
